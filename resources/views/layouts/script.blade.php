1f643cb1027c196e0e40b0295ec261959185a7a4
harjot656 <harjot656@gmail.com>
2018-12-17 22:39:24 +0530

    Update script.blade.php

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,10 +1,10 @@
-<script type="text/javascript" src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
-        <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
-		<script type="text/javascript" src="{{asset('js/jquery.slimscroll.js')}}"></script>
-		<script type="text/javascript" src="{{asset('js/select2.min.js')}}"></script>
-		<script type="text/javascript" src="{{asset('js/moment.min.js')}}"></script>
-		<script type="text/javascript" src="{{asset('js/bootstrap-datetimepicker.min.js')}}"></script>
-		<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
-		<script type="text/javascript" src="{{asset('js/jquery.validate.min.js')}}"></script>
-		<script type="text/javascript" src="{{asset('js/additional-methods.js')}}"></script>
-		<script type="text/javascript" src="{{asset('js/add_employee.js')}}"></script>
+<script type="text/javascript" src="{{secure_asset('js/jquery-3.2.1.min.js')}}"></script>
+        <script type="text/javascript" src="{{secure_asset('js/bootstrap.min.js')}}"></script>
+		<script type="text/javascript" src="{{secure_asset('js/jquery.slimscroll.js')}}"></script>
+		<script type="text/javascript" src="{{secure_asset('js/select2.min.js')}}"></script>
+		<script type="text/javascript" src="{{secure_asset('js/moment.min.js')}}"></script>
+		<script type="text/javascript" src="{{secure_asset('js/bootstrap-datetimepicker.min.js')}}"></script>
+		<script type="text/javascript" src="{{secure_asset('js/app.js')}}"></script>
+		<script type="text/javascript" src="{{secure_asset('js/jquery.validate.min.js')}}"></script>
+		<script type="text/javascript" src="{{secure_asset('js/additional-methods.js')}}"></script>
+		<script type="text/javascript" src="{{secure_asset('js/add_employee.js')}}"></script>
