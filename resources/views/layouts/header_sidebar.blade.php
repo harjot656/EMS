17251684dba9bb4ff49fc6c42b23aac0db8003c8
harjot656 <harjot656@gmail.com>
2018-12-17 18:47:22 +0530

    Update header_sidebar.blade.php

--- resources/views/layouts/header_sidebar.blade.php
+++ resources/views/layouts/header_sidebar.blade.php
@@ -1,17 +1,17 @@
 <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-        <link rel="shortcut icon" type="image/x-icon" href="{{asset('img/favicon.png')}}">
+        <link rel="shortcut icon" type="image/x-icon" href="{{secure_asset('img/favicon.png')}}">
         <title>Employees - HRMS admin template</title>
 		<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{asset('css/font-awesome.min.css')}}">
-		<link rel="stylesheet" type="text/css" href="{{asset('css/select2.min.css')}}">
-		<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap-datetimepicker.min.css')}}">
-        <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{secure_asset('css/bootstrap.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{secure_asset('css/font-awesome.min.css')}}">
+		<link rel="stylesheet" type="text/css" href="{{secure_asset('css/select2.min.css')}}">
+		<link rel="stylesheet" type="text/css" href="{{secure_asset('css/bootstrap-datetimepicker.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{secure_asset('css/style.css')}}">
 		<!--[if lt IE 9]>
-			<script src="assets/js/php5shiv.min.js"></script>
-			<script src="assets/js/respond.min.js"></script>
+			<script src="secure_assets/js/php5shiv.min.js"></script>
+			<script src="secure_assets/js/respond.min.js"></script>
 		<![endif]-->
     </head>
     <body>
@@ -20,7 +20,7 @@
             <div class="header">
                 <div class="header-left">
                     <a href="index.php" class="logo">
-						<img src="{{asset('img/logo.png')}}" width="40" height="40" alt="">
+						<img src="{{secure_asset('img/logo.png')}}" width="40" height="40" alt="">
 					</a>
                 </div>
                 <div class="page-title-box pull-left">
@@ -32,7 +32,7 @@
 						
 					<li class="dropdown">
 						<a href="{{route('profile')}}" class="dropdown-toggle user-link" data-toggle="dropdown" title="Admin">
-							<span class="user-img"><img class="img-circle" src="{{asset('img/user.jpg')}}" width="40" alt="Admin">
+							<span class="user-img"><img class="img-circle" src="{{secure_asset('img/user.jpg')}}" width="40" alt="Admin">
 							<span class="status online"></span></span>
 							<span>Admin</span>
 							<i class="caret"></i>
